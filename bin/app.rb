dd86658ffad9d7be41b0e91e269538474456ae75
bendee48 <benbanj@hotmail.com>
2019-01-16 21:15:34 +0000

    Adding views

--- bin/app.rb
+++ bin/app.rb
@@ -6,10 +6,19 @@ set :public_folder, "static"
 set :views, "views"
 
 get '/' do
-    return 'Hello World'
+    return 'Hello World IIIIIIIIII'
 end
 
 get '/hello/' do
     greeting = params[:greeting] || "Hi There"
     erb :index, :locals => {'greeting' => greeting}
+
+end
+
+get '/biteme/:name' do |x|
+  "Bite me #{x}."
+end
+
+get '/splatty/*.*' do
+  params['splat'][0]
 end
