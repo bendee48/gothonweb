cd162e7ab0a7e8202fa81f37e6a79fd028aa9e9e
bendee48 <benbanj@hotmail.com>
2019-02-07 20:44:10 +0000

    Updating map

--- lib/gothonweb/map.rb
+++ lib/gothonweb/map.rb
@@ -1,19 +1,79 @@
-class Room
+module Map
 
-  def initialize(name, description)
-    @name = name
-    @description = description
-    @paths = {}
+  class Room
+
+    def initialize(name, description)
+      @name = name
+      @description = description
+      @paths = {}
+    end
+
+    attr_accessor :name, :description, :paths
+
+    def go(direction)
+      @paths[direction]
+    end
+
+    def add_paths(paths)
+      @paths.update(paths)
+    end
+
   end
 
-  attr_accessor :name, :description, :paths
+  CENTRAL_CORRIDOR = Room.new("Central Corridor", "This is the central corrdior")
+  LASER_WEAPON_ARMORY = Room.new("Laser Weapon Armory", "This is the laser weapon armory")
+  THE_BRIDGE = Room.new("The Bridge", "This is the bridge")
+  ESCAPE_POD = Room.new("Escape Pod", "This is the escape pod")
+  THE_END_WINNER = Room.new("The End", "This is the end if you win")
+  THE_END_LOSER = Room.new("The End", "This is the end if you lose")
 
-  def go(direction)
-    @paths[direction]
+  ESCAPE_POD.add_paths({
+    "2" => THE_END_WINNER,
+    "*" => THE_END_LOSER
+    })
+
+  GENERIC_DEATH = Room.new("death", "You died.")
+
+  THE_BRIDGE.add_paths({
+    "throw the bomb" => GENERIC_DEATH,
+    "slowly place the bomb" => ESCAPE_POD
+    })
+
+  LASER_WEAPON_ARMORY.add_paths({
+        '0132' => THE_BRIDGE,
+        '*' => GENERIC_DEATH
+    })
+
+  CENTRAL_CORRIDOR.add_paths({
+        'shoot!' => GENERIC_DEATH,
+        'dodge!' => GENERIC_DEATH,
+        'tell a joke' => LASER_WEAPON_ARMORY
+    })
+
+  START = CENTRAL_CORRIDOR
+
+  # A whitelist of allowed room names. We use this so that
+    # bad people on the internet can't access random variables
+    # with names.  You can use Test::constants and Kernel.const_get
+    # too.
+  ROOM_NAMES = {
+      'CENTRAL_CORRIDOR' => CENTRAL_CORRIDOR,
+      'LASER_WEAPON_ARMORY' => LASER_WEAPON_ARMORY,
+      'THE_BRIDGE' => THE_BRIDGE,
+      'ESCAPE_POD' => ESCAPE_POD,
+      'THE_END_WINNER' => THE_END_WINNER,
+      'THE_END_LOSER' => THE_END_LOSER,
+      'START' => START,
+  }
+
+  def Map::load_room(session)
+      # Given a session this will return the right room or nil
+      return ROOM_NAMES[session[:room]]
   end
 
-  def add_paths(paths)
-    @paths.update(paths)
+  def Map::save_room(session, room)
+      # Store the room in the session for later, using its name
+      session[:room] = ROOM_NAMES.key(room)
   end
 
 end
--- tests/test_map.rb
+++ tests/test_map.rb
@@ -5,46 +5,64 @@ require "rack/test"
 class TestMap < Test::Unit::TestCase
   include Rack::Test::Methods
 
-  def test_room
-    gold = Room.new("Gold Room", "This room has gold in it you can grab.")
-    assert_equal("Gold Room", gold.name)
-    assert_equal({}, gold.paths)
-  end
+  def test_room()
+        gold = Map::Room.new("GoldMap::Room",
+                    """This room has gold in it you can grab. There's a
+                door to the north.""")
+        assert_equal( "GoldMap::Room", gold.name)
+        assert_equal({}, gold.paths)
+    end
 
-  def test_room_paths
-    center = Room.new("Center", "Test room in the center.")
-    north = Room.new("North", "Test room in the north.")
-    south = Room.new("South", "Test room in the south.")
+    def test_room_paths()
+        center = Map::Room.new("Center", "Test room in the center.")
+        north = Map::Room.new("North", "Test room in the north.")
+        south = Map::Room.new("South", "Test room in the south.")
 
-    center.add_paths({'north' => north, 'south'=> south})
-    assert_equal(north, center.go('north'))
-    assert_equal(south, center.go('south'))
-  end
+        center.add_paths({'north'=> north, 'south'=> south})
+        assert_equal(north, center.go('north'))
+        assert_equal(south, center.go('south'))
 
-  def test_map
-    start = Room.new("Start", "You can go west and down a hole.")
-    west = Room.new("Trees", "There are trees here and you can go east.")
-    down = Room.new("Dungeon", "It's dark down here, you can go up.")
+    end
 
-    start.add_paths({'west'=> west, 'down'=> down})
-    west.add_paths({'east'=> start})
-    down.add_paths({'up'=> start})
+    def test_map()
+        start = Map::Room.new("Start", "You can go west and down a hole.")
+        west = Map::Room.new("Trees", "There are trees here, you can go east.")
+        down = Map::Room.new("Dungeon", "It's dark down here, you can go up.")
 
-    assert_equal(west, start.go('west'))
-    assert_equal(start, start.go('west').go('east'))
-    assert_equal(start, start.go('down').go('up'))
-  end
+        start.add_paths({'west'=> west, 'down'=> down})
+        west.add_paths({'east'=> start})
+        down.add_paths({'up'=> start})
 
-  def test_mygame
-    lev1 = Room.new("Level 1", "Where you battle Depression.")
-    lev1_map = {'level1' => lev1}
+        assert_equal(west, start.go('west'))
+        assert_equal(start, start.go('west').go('east'))
+        assert_equal(start, start.go('down').go('up'))
+    end
 
-    assert_equal(lev1.name, "Level 1")
-    assert_equal(lev1.description, "Where you battle Depression.")
+    def test_gothon_game_map()
+        assert_equal(Map::GENERIC_DEATH, Map::START.go('shoot!'))
+        assert_equal(Map::GENERIC_DEATH, Map::START.go('dodge!'))
 
-    lev1.add_paths(lev1_map)
-    assert_equal(lev1, lev1.go('level1'))
+        room = Map::START.go('tell a joke')
+        assert_equal(Map::LASER_WEAPON_ARMORY, room)
 
-  end
+        # complete this test by making it play the game
+    end
+
+    def test_session_loading()
+        session = {}
+
+        room = Map::load_room(session)
+        assert_equal(room, nil)
+
+        Map::save_room(session, Map::START)
+        room = Map::load_room(session)
+        assert_equal(room, Map::START)
+
+        room = room.go('tell a joke')
+        assert_equal(room, Map::LASER_WEAPON_ARMORY)
+
+        Map::save_room(session, room)
+        assert_equal(room, Map::LASER_WEAPON_ARMORY)
+    end
 
 end
